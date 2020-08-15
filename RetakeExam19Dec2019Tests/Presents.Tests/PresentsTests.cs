8231e8104d45e175343417faa8a90e62b5bb99d9
Dobromir Petrov <luzterin@gmail.com>
2020-08-15 13:09:26 +0300

    final

--- RetakeExam19Dec2019Tests/Presents.Tests/PresentsTests.cs
+++ RetakeExam19Dec2019Tests/Presents.Tests/PresentsTests.cs
@@ -1,8 +1,137 @@
-﻿namespace Presents.Tests
+﻿using NUnit.Framework;
+
+namespace Presents.Tests
 {
     using System;
 
     public class PresentsTests
     {
+        private Bag bag;
+        private Present presentOne;
+        [SetUp]
+        public void Initialize()
+        {
+            this.bag = new Bag();
+            this.presentOne = new Present("Ivan", 300);
+        }
+        [Test]
+        public void TestPresentConstructorAndGetterName()
+        {
+            string expectedName = "Pesho";
+            double expectedMagic = 200;
+            Present present = new Present(expectedName, expectedMagic);
+
+            Assert.AreEqual(expectedName, present.Name);
+        }
+        [Test]
+        public void TestPresentConstructorAndGetterMagic()
+        {
+            string expectedName = "Pesho";
+            double expectedMagic = 200;
+            Present present = new Present(expectedName, expectedMagic);
+
+            Assert.AreEqual(expectedMagic, present.Magic);
+        }
+        [Test]
+        public void TestPresentConstructorAndAndSetterName()
+        {
+            string name = "Pesho";
+            double magic = 200;
+            Present present = new Present(name, magic);
+            string expectedName = "Ivan";
+            present.Name = expectedName;
+            Assert.AreEqual(expectedName, present.Name);
+        }
+        [Test]
+        public void TestPresentConstructorAndAndSetterMagic()
+        {
+            string name = "Pesho";
+            double magic = 200;
+            Present present = new Present(name, magic);
+            double expectedMagic = 300;
+
+            present.Magic = expectedMagic;
+            Assert.AreEqual(expectedMagic, present.Magic);
+        }
+
+        [Test]
+        public void TestBagConstructorAndCreateAndGetPresents()
+        {
+            bag.Create(presentOne);
+            Assert.AreEqual(1, bag.GetPresents().Count);
+        }
+
+        [Test]
+        public void TestBagConstructorNull()
+        {
+            Assert.IsNotNull(this.bag.GetPresents());
+        }
+        [Test]
+        public void TestBagCreateExceptionNullPresent()
+        {
+            Present nullPresent = null;
+            Assert.That(() =>
+                {
+                    this.bag.Create(nullPresent);
+
+                },Throws.ArgumentNullException
+                    .With.Message.EqualTo("Value cannot be null. (Parameter 'Present is null')"));
+
+
+        }
+        [Test]
+        public void TestBagCreateExceptionNullPresentExist()
+        {
+            this.bag.Create(presentOne);
+            Present anotherPresent = presentOne;
+            Assert.That(() =>
+            {
+                this.bag.Create(anotherPresent);
+
+            }, Throws.InvalidOperationException
+                .With.Message.EqualTo("This present already exists!"));
+        }
+
+        [Test]
+        public void TestRemovePresent()
+        {
+            this.bag.Create(presentOne);
+            Present anotherPresent = new Present("Gosho",250);
+            this.bag.Create(anotherPresent);
+            
+            Assert.AreEqual(true, this.bag.Remove(anotherPresent));
+        }
+        [Test]
+        public void TestRemovePresentCount()
+        {
+            this.bag.Create(presentOne);
+            Present anotherPresent = new Present("Gosho", 250);
+            this.bag.Create(anotherPresent);
+            this.bag.Remove(anotherPresent);
+            Assert.AreEqual(1, this.bag.GetPresents().Count);
+        }
+
+        [Test]
+        public void TestGetPresentWithLeastMagic()
+        {
+            //Present present = this.presents.OrderBy(p => p.Magic).First();
+            Present anotherPresent = new Present("Gosho", 10);
+            this.bag.Create(presentOne);
+            this.bag.Create(anotherPresent);
+            Present acutalPresent = this.bag.GetPresentWithLeastMagic();
+            Assert.AreEqual(10, acutalPresent.Magic);
+        }
+
+        [Test]
+        public void TestGetPresent()
+        {
+            //Present present = this.presents.FirstOrDefault(p => p.Name == name);
+            Present anotherPresent = new Present("Gosho", 10);
+            this.bag.Create(presentOne);
+            this.bag.Create(anotherPresent);
+            var actualPresent = this.bag.GetPresent("Gosho");
+            Assert.AreEqual(10, anotherPresent.Magic);
+
+        }
     }
 }
